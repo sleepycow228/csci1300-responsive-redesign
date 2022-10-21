378f1120ad37914117b1a39f099ea4951fd6781f
sleepycow228 <115034917+sleepycow228@users.noreply.github.com>
2022-10-20 21:18:19 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// const handlePersonaClick = () => {
+//   newEle = document.createElement('h2');
+//   newEle.style.backgroundColor = 'white';
+
+
+//   // TODO: Append your new element to the page
+//   existingElement = document.getElementById('button-response');
+//   existingElement.append(newEle);
+
+// }
